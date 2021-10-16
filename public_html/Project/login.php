caf37f862a5b2909b704f722eb040dbe37b35a35
lenahxo <holseyalen.l@gmail.com>
2021-10-16 17:32:11 -0400

    Adding a Username

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -65,7 +65,7 @@ require(__DIR__."/../../partials/nav.php");?>
          //TODO 4
         $db = getDB();
 
-        $stmt = $db->prepare("SELECT email, password FROM Users WHERE email = :email");
+        $stmt = $db->prepare("SELECT id, email, password FROM Users WHERE email = :email");
         try
         {
             $r = $stmt->execute([":email" => $email]);
