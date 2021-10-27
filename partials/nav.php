c14c5afa3632826102134a5484e335998e8e3243
lenahxo <holseyalen.l@gmail.com>
2021-10-27 17:19:06 -0400

    Updating Navi

--- partials/nav.php
+++ partials/nav.php
@@ -1,15 +1,14 @@
-
 <?php
 //Note: this is to resolve cookie issues with port numbers
 $domain = $_SERVER["HTTP_HOST"];
 if (strpos($domain, ":")) {
     $domain = explode(":", $domain)[0];
 }
+$localWorks = true; //some people have issues with localhost for the cookie params
+//if you're one of those people make this false
 
-$localWorks = true;
-
-if (($localWorks && $domain == "localhost") || $domain != "localhost")
-{
+//this is an extra condition added to "resolve" the localhost issue for the session cookie
+if (($localWorks && $domain == "localhost") || $domain != "localhost") {
     session_set_cookie_params([
         "lifetime" => 60 * 60,
         "path" => "/Project",
@@ -20,7 +19,6 @@ if (($localWorks && $domain == "localhost") || $domain != "localhost")
         "samesite" => "lax"
     ]);
 }
-
 session_start();
 require_once(__DIR__ . "/../lib/functions.php");
 
@@ -37,6 +35,11 @@ require_once(__DIR__ . "/../lib/functions.php");
         <?php if (!is_logged_in()) : ?>
             <li><a href="<?php echo get_url('login.php'); ?>">Login</a></li>
             <li><a href="<?php echo get_url('register.php'); ?>">Register</a></li>
+        <?php endif; ?>
+        <?php if (has_role("Admin")) : ?>
+            <li><a href="<?php echo get_url('admin/create_role.php'); ?>">Create Role</a></li>
+            <li><a href="<?php echo get_url('admin/list_roles.php'); ?>">List Roles</a></li>
+            <li><a href="<?php echo get_url('admin/assign_roles.php'); ?>">Assign Roles</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()) : ?>
             <li><a href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
