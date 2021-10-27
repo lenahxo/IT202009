a13c7544b7fd08f9e74cdbe6a87b538178544a61
lenahxo <holseyalen.l@gmail.com>
2021-10-27 17:06:00 -0400

    Using roles and fixing paths

--- partials/nav.php
+++ partials/nav.php
@@ -5,30 +5,41 @@ $domain = $_SERVER["HTTP_HOST"];
 if (strpos($domain, ":")) {
     $domain = explode(":", $domain)[0];
 }
-session_set_cookie_params([
-    "lifetime" => 60 * 60,
-    "path" => "/Project",
-    //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
-    "domain" => $domain,
-    "secure" => true,
-    "httponly" => true,
-    "samesite" => "lax"
-]);
+
+$localWorks = true;
+
+if (($localWorks && $domain == "localhost") || $domain != "localhost")
+{
+    session_set_cookie_params([
+        "lifetime" => 60 * 60,
+        "path" => "/Project",
+        //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
+        "domain" => $domain,
+        "secure" => true,
+        "httponly" => true,
+        "samesite" => "lax"
+    ]);
+}
+
 session_start();
 require_once(__DIR__ . "/../lib/functions.php");
 
 ?>
+<!-- include css and js files -->
+<link rel="stylesheet" href="<?php echo get_url('styles.css'); ?>">
+<script src="<?php echo get_url('helpers.js'); ?>"></script>
 <nav>
     <ul>
         <?php if (is_logged_in()) : ?>
-            <li><a href="home.php">Home</a></li>
+            <li><a href="<?php echo get_url('home.php'); ?>">Home</a></li>
+            <li><a href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
-            <li><a href="login.php">Login</a></li>
-            <li><a href="register.php">Register</a></li>
+            <li><a href="<?php echo get_url('login.php'); ?>">Login</a></li>
+            <li><a href="<?php echo get_url('register.php'); ?>">Register</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()) : ?>
-            <li><a href="logout.php">Logout</a></li>
+            <li><a href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
         <?php endif; ?>
     </ul>
 </nav>
