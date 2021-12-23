318eb053c0759560576d149b507fbe58d050189b
lenahxo <holseyalen.l@gmail.com>
2021-12-22 23:54:57 -0500

    Top 10 competition scores

--- lib/functions.php
+++ lib/functions.php
@@ -262,7 +262,7 @@ function join_comp($comp_id, $user_id, $cost)
     if ($comp_id > 0) {
         try {
             $cost = (int)se($_POST, "cost", 0, false);
-            $name = se($_POST, "comp_name", "", false);
+            $name = se($_POST, "compName", "", false);
             if ($bal >= $cost) {
                 if (point_change($cost, "join_comp", $user_id)) {
                     if (add_to_comp($comp_id, $user_id)) {
@@ -316,6 +316,43 @@ function save_score($score, $user_id, $showFlash = false)
         flash("Error saving score: " . var_export($e->errorInfo, true), "danger");
     }
 }
+
+
+//top 10 competition scores
+function get_top_scores_for_comp($comp_id, $limit = 10)
+{
+    $db = getDB();
+    //below if a user can win more than one place
+    /*$stmt = $db->prepare(
+        "SELECT score, s.created, username, u.id as user_id FROM BGD_Scores s 
+    JOIN BGD_UserComps uc on uc.user_id = s.user_id 
+    JOIN BGD_Competitions c on c.id = uc.competition_id
+    JOIN Users u on u.id = s.user_id WHERE c.id = :cid AND s.score >= c.min_score AND s.created 
+    BETWEEN uc.created AND c.expires ORDER BY s.score desc LIMIT :limit"
+    );*/
+    //Below if a user can't win more than one place
+    $stmt = $db->prepare("SELECT * FROM (SELECT s.user_id, s.score,s.created, a.id as account_id, DENSE_RANK() OVER (PARTITION BY s.user_id ORDER BY s.score desc) as `rank` FROM SCORES s
+    JOIN Participants uc on uc.user_id = s.user_id
+    JOIN Competitions c on uc.comp_id = c.id
+    JOIN Users a on a.user_id = s.user_id
+    WHERE c.id = :cid AND s.created BETWEEN uc.created AND c.expires
+    )as t where `rank` = 1 ORDER BY score desc LIMIT :limit");
+    $scores = [];
+    try {
+        $stmt->bindValue(":cid", $comp_id, PDO::PARAM_INT);
+        $stmt->bindValue(":limit", $limit, PDO::PARAM_INT);
+        $stmt->execute();
+        $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if ($r) {
+            $scores = $r;
+        }
+    } catch (PDOException $e) {
+        flash("There was a problem fetching scores, please try again later", "danger");
+        error_log("List competition scores error: " . var_export($e, true));
+    }
+    return $scores;
+}
+
 
 //best score
 function get_best_score($user_id)
